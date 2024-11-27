a6e68f07cb76ea0c79dbea458d7ab3cf584c8d7e
Athen Lee <cupid.jack505@gmail.com>
2024-11-27 08:30:55 -0500

    out team photo modify

--- src/components/Blog/blogData.tsx
+++ src/components/Blog/blogData.tsx
@@ -6,7 +6,7 @@ const blogData: Blog[] = [
     title: "John Doe",
     paragraph:
       "John brings over 15 years of experience in consulting, leading our team to deliver strategic solutions aligned with client goals.",
-    image: "/images/blog/blog-01.jpg",
+    image: "/images/blog/blog-01.png",
     author: {
       name: "John Doe",
       image: "/images/blog/author-01.png",
@@ -20,7 +20,7 @@ const blogData: Blog[] = [
     title: "Jane Smith",
     paragraph:
       "With 10+ years in IT consulting, Jane helps businesses optimize technology infrastructure with cutting-edge solutions.",
-    image: "/images/blog/blog-02.jpg",
+    image: "/images/blog/blog-02.png",
     author: {
       name: "Musharof Chy",
       image: "/images/blog/author-02.png",
@@ -34,7 +34,7 @@ const blogData: Blog[] = [
     title: "Robert Brown",
     paragraph:
       "Robert ensures success of clients by managing projects and fostering long-term relationships for sustained business growth.",
-    image: "/images/blog/blog-03.jpg",
+    image: "/images/blog/blog-03.png",
     author: {
       name: "Lethium Deo",
       image: "/images/blog/author-03.png",
